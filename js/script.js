dbff7d65eae78f055124527bcde8761b04c4a830
mTutM <maks.tutukov@mail.ru>
2021-04-19 20:53:33 +0300

    update project

--- js/script.js
+++ js/script.js
@@ -43,10 +43,12 @@ document.addEventListener('DOMContentLoaded', () => {
     // bonus game
     const bonusGameBtnElem = document.getElementById('bonusGame_btn');
     const imgRulesElem = document.querySelector('.img_rules');
+    const scoreNumberElem = document.querySelector('.score_number');
     const body = document.body;
 
     bonusGameBtnElem.addEventListener('click', () => {
         body.classList.toggle('bonus_game');
+        scoreNumberElem.textContent = '0';
         if (bonusGameBtnElem.textContent == 'rps') {
             imgRulesElem.src = 'images/image-rules-bonus.svg';
             bonusGameBtnElem.textContent = 'rpsls';
@@ -55,4 +57,60 @@ document.addEventListener('DOMContentLoaded', () => {
             imgRulesElem.src = 'images/image-rules.svg';
         }
     });
+
+    // //////////////////////////////
+    // game
+    const game = () => {
+        const optionElem = document.querySelectorAll('.option');
+        const mainElem = document.querySelector('.main');
+        const mainGameElem = document.querySelector('.main_game');
+        const youPickedBlockElem = document.querySelector('.you_picked_block');
+        const playAgainElem = document.getElementById('play_again');
+        let btnElem;
+        let truefalse = 1;
+        const youWin = 'You Win';
+        const youLose = 'You Lose';
+        const itsATie = "IT'S A TIE";
+
+        optionElem.forEach((item) => {
+            item.addEventListener('click', () => {
+                if (truefalse) {
+                    mainElem.style.opacity = '0';
+
+                    setTimeout(() => {
+                        mainElem.style.display = 'none';
+                    }, 500);
+
+                    mainGameElem.style.display = 'flex';
+
+                    setTimeout(() => {
+                        mainGameElem.style.opacity = '1';
+                    }, 800);
+
+                    youPickedBlockElem.innerHTML = item.outerHTML;
+                    btnElem = item.dataset.option;
+                    truefalse = 0;
+                }
+            });
+        });
+
+        playAgainElem.addEventListener('click', () => {
+            mainGameElem.style.opacity = '0';
+            setTimeout(() => {
+                mainGameElem.style.display = 'none';
+            }, 500);
+
+            setTimeout(() => {
+                mainElem.style.display = 'block';
+            }, 500);
+
+            setTimeout(() => {
+                mainElem.style.opacity = '1';
+            }, 800);
+
+            truefalse = 1;
+        });
+    };
+
+    game();
 });
