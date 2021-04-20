54d2853d259619d7f98a2214d62d4d8d70647ebd
mTutM <maks.tutukov@mail.ru>
2021-04-20 11:20:04 +0300

    update html, css and added function winLoseTie js

--- js/script.js
+++ js/script.js
@@ -68,20 +68,80 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // //////////////////////////////
     // game
-    const game = () => {
-        const optionElem = document.querySelectorAll('.option');
-        const mainElem = document.querySelector('.main');
-        const mainGameElem = document.querySelector('.main_game');
-        const youPickedBlockElem = document.querySelector('.you_picked_block');
-        const theHousePickedBlockElem = document.querySelector('.the_house_picked_block');
-        const playAgainElem = document.getElementById('play_again');
-        const totalTextElem = document.getElementById('total_text');
-        let btnElem;
-        let truefalse = 1;
-        const youWin = 'You Win';
-        const youLose = 'You Lose';
-        const itsATie = "IT'S A TIE";
+    const optionElem = document.querySelectorAll('.option');
+    const mainElem = document.querySelector('.main');
+    const mainGameElem = document.querySelector('.main_game');
+    const newGameBlockElem = document.querySelector('.new_game_block');
+    const youPickedBlockElem = document.querySelector('.you_picked_block');
+    const theHousePickedBlockElem = document.querySelector('.the_house_picked_block');
+    const playAgainElem = document.getElementById('play_again');
+    const totalTextElem = document.getElementById('total_text');
+    let btnElem;
+    let truefalse = 1;
+    const youWin = 'You Win';
+    const youLose = 'You Lose';
+    const itsATie = "IT'S A TIE";
 
+    function randomInteger(min, max) {
+        let rand = min + Math.random() * (max + 1 - min);
+        return Math.floor(rand);
+    }
+
+    function winLoseTie(win, lose) {
+        let rand = randomInteger(0, 2);
+        console.log(optionElem);
+        youPickedBlockElem.classList.remove('win_option');
+        theHousePickedBlockElem.classList.remove('win_option');
+        if (optionElem[rand].dataset.option == win) {
+            totalTextElem.textContent = youWin;
+            setTimeout(() => {
+                youPickedBlockElem.classList.add('win_option');
+                scoreNumberElem.textContent = Number(scoreNumberElem.textContent) + Number(1);
+            }, 3000);
+        } else if (optionElem[rand].dataset.option == lose) {
+            totalTextElem.textContent = youLose;
+            setTimeout(() => {
+                theHousePickedBlockElem.classList.add('win_option');
+                scoreNumberElem.textContent = '0';
+            }, 3000);
+        } else {
+            totalTextElem.textContent = itsATie;
+        }
+
+        setTimeout(() => {
+            theHousePickedBlockElem.innerHTML = '<div></div>' + optionElem[rand].outerHTML;
+        }, 1500);
+
+        setTimeout(() => {
+            mainGameElem.classList.add('active_new_game');
+        }, 2000);
+
+        setTimeout(() => {
+            newGameBlockElem.style.opacity = '1';
+        }, 3000);
+    }
+
+    const gameLogic = (btn) => {
+        if (bonusGameBtnElem.textContent == 'rps') {
+            switch (btn) {
+                case 'paper-option':
+                    winLoseTie('rock-option', 'scissors-option');
+                    break;
+
+                case 'scissors-option':
+                    winLoseTie('paper-option', 'rock-option');
+                    break;
+
+                case 'rock-option':
+                    winLoseTie('scissors-option', 'paper-option');
+                    break;
+            }
+        } else {
+            let rand = randomInteger(0, 4);
+        }
+    };
+
+    const game = () => {
         optionElem.forEach((item) => {
             item.addEventListener('click', () => {
                 if (truefalse) {
@@ -97,8 +157,8 @@ document.addEventListener('DOMContentLoaded', () => {
                         mainGameElem.style.opacity = '1';
                     }, 600);
 
-                    youPickedBlockElem.innerHTML = item.outerHTML;
-                    btnElem = item.dataset.option;
+                    youPickedBlockElem.innerHTML = '<div></div>' + item.outerHTML;
+                    gameLogic(item.dataset.option);
                     truefalse = 0;
                 }
             });
@@ -118,6 +178,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 mainElem.style.opacity = '1';
             }, 600);
 
+            totalTextElem.textContent = '';
+            mainGameElem.classList.remove('active_new_game');
+            newGameBlockElem.style.opacity = '0';
+            theHousePickedBlockElem.innerHTML = '<div class="black_circle"></div>';
             truefalse = 1;
         });
     };
